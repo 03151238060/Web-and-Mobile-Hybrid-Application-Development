1fe24f0e377a2fa25cb70e68b859d230daa0cf96
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-05 15:01:55 +0500

    in progress

--- objects_sirghous/fb.js
+++ objects_sirghous/fb.js
@@ -1,12 +1,11 @@
-// function data(){
-//     var name=document.getElementById('name')
-//     var roll=document.getElementById('roll')
+function data(){
+    var name=document.getElementById('name')
+    var roll=document.getElementById('roll')
 
-//     var Student={
-//         name:name.value,
-//         roll:roll.value
-//     }
-//     console.log(Student)
-// }
-
-console.log(firebase.database())
+    var Student={
+        name:name.value,
+        roll:roll.value
+    }
+    // console.log(Student)
+    firebase.database().ref('student').set(Student)
+}
