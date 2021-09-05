e730d5fa598e997299da429afc251f55b0d0d465
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-05 14:42:48 +0500

    done

---
+++ objects_sirghous/fb.js
@@ -0,0 +1,12 @@
+// function data(){
+//     var name=document.getElementById('name')
+//     var roll=document.getElementById('roll')
+
+//     var Student={
+//         name:name.value,
+//         roll:roll.value
+//     }
+//     console.log(Student)
+// }
+
+console.log(firebase.database())
