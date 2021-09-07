36083e0628d2e88ddaa8e2991c73347c1ed4c1c2
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-07 14:26:13 +0500

    done

--- objects_sirghous/fb.js
+++ objects_sirghous/fb.js
@@ -2,10 +2,23 @@ function data(){
     var name=document.getElementById('name')
     var roll=document.getElementById('roll')
 
+    var key=Math.random()*923032;
     var Student={
         name:name.value,
-        roll:roll.value
+        roll:roll.value,
+        key:key.toFixed()
     }
     // console.log(Student)
-    firebase.database().ref('student').set(Student)
+    // firebase.database().ref('student').set(Student)
+    // firebase.database().ref('student').push(Student)
+    // firebase.database().ref('student').child('mystudents/today').push(Student)
+    // console.log(key.toFixed())
+    firebase.database().ref('student/'+key.toFixed()).set(Student)
 }
+
+function getFirebaseData(){
+    firebase.database().ref('student').once('value',function(data){
+        console.log(data.val());
+    })
+}
+getFirebaseData()
