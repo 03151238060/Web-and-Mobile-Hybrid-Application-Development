59ae91da06d4226b1eb585b5f32383f04a605ee6
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-08 23:47:12 +0500

    done

--- objects_sirghous/fb.js
+++ objects_sirghous/fb.js
@@ -1,24 +1,46 @@
-function data(){
-    var name=document.getElementById('name')
-    var roll=document.getElementById('roll')
+function data() {
+    var name = document.getElementById('name')
+    var roll = document.getElementById('roll')
 
-    var key=Math.random()*923032;
-    var Student={
-        name:name.value,
-        roll:roll.value,
-        key:key.toFixed()
+    var key = Math.random() * 923032;
+    var Student = {
+        name: name.value,
+        roll: roll.value,
+        key: key.toFixed()
     }
     // console.log(Student)
     // firebase.database().ref('student').set(Student)
     // firebase.database().ref('student').push(Student)
     // firebase.database().ref('student').child('mystudents/today').push(Student)
     // console.log(key.toFixed())
-    firebase.database().ref('student/'+key.toFixed()).set(Student)
+    firebase.database().ref('student/' + key.toFixed()).set(Student)
 }
 
-function getFirebaseData(){
-    firebase.database().ref('student').once('value',function(data){
+function getFirebaseData() {
+    // firebase.database().ref('student').once('value',function(data){
+    //     console.log(data.val());
+    // })
+    // firebase.database().ref('student/403923').once('value',function(data){
+    //     console.log(data.val());
+    // })
+
+    firebase.database().ref('student').on('child_added', function (data) {
         console.log(data.val());
     })
 }
 getFirebaseData()
+
+function removeFirebaseData() {
+    firebase.database().ref('student/675679').remove();
+}
+
+// window.onload=removeFirebaseData()
+
+function editFirebaseData(){
+    firebase.database().ref('student/351105').set({
+        name:"wajahat",
+        roll:2334,
+        key:351105
+    })
+}
+editFirebaseData()
