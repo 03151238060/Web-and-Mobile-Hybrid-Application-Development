7ebb2b70d8530fc8213fae5037c564f2f3f38f05
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-19 12:40:06 +0500

    done

---
+++ JavaScript/reverse.js
@@ -0,0 +1,11 @@
+function reverse(str){
+    var reversed='';
+    for( var i=str.length-1;i>=0;i--){
+        reversed+=str[i];
+
+    }
+    console.log(str);
+    console.log(reversed)
+}
+
+reverse('sumama')
